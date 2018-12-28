11f2c1f4f42fe92a334130b468aa50540527aac4
Kuro <Kuro@Kuro>
2018-12-28 01:37:47 +0100

    ajout de commentaires initialisation de la pioche

--- src/main/vue/Menu.java
+++ src/main/vue/Menu.java
@@ -8,50 +8,61 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import main.Joueur;
 import main.planche.PlancheDomino;
 
 @SuppressWarnings("serial")
 public class Menu extends JFrame {
-	public Menu(){
+	public Menu() {
 		this.pack();
 		this.setSize(new Dimension(1920, 1080));
 		this.setVisible(true);
 		this.setTitle("Menu");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new GridLayout(2, 4));
+		
 		JButton domino = new JButton("Domino");
 		JButton puzzle = new JButton("Puzzle");
 		JButton dominoGommettes = new JButton("Domino-gommettes");
 		JButton saboteur = new JButton("Saboteur");
+		
 		JPanel imageDomino = new ImagePan("Images/domino.jpg");
 		this.add(imageDomino, BorderLayout.CENTER);
+		
 		JPanel imagePuzzle = new ImagePan("Images/puzzle.jpg");
 		this.add(imagePuzzle, BorderLayout.CENTER);
+		
 		JPanel imageDominoGommettes = new ImagePan("Images/dominoGommettes.jpg");
 		this.add(imageDominoGommettes, BorderLayout.CENTER);
+		
 		JPanel imageSaboteur = new ImagePan("Images/Saboteur.jpg");
 		this.add(imageSaboteur, BorderLayout.CENTER);
+		
 		domino.addActionListener(event -> {
 			PlancheDomino planche = new PlancheDomino();
-			VueDomino vue = new VueDomino(planche);
+			VueDomino vue = new VueDomino(planche, new Joueur("joueur 1"));
+			this.setVisible(false);
 		});
 		this.add(domino);
-		
+
 		puzzle.addActionListener(event -> {
 			PlancheDomino planche = new PlancheDomino();
-			Vue vue = new Vue(planche);
+			// Vue vue = new Vue(planche, new Joueur("joueur 1"));
+			this.setVisible(false);
 		});
 		this.add(puzzle);
-		
+
 		dominoGommettes.addActionListener(event -> {
 			PlancheDomino planche = new PlancheDomino();
-			Vue vue = new Vue(planche);
+			// Vue vue = new Vue(planche, new Joueur("joueur 1"));
+			this.setVisible(false);
 		});
 		this.add(dominoGommettes);
-		
+
 		saboteur.addActionListener(event -> {
 			PlancheDomino planche = new PlancheDomino();
-			Vue vue = new Vue(planche);
+			// Vue vue = new Vue(planche, new Joueur("joueur 1"));
+			this.setVisible(false);
 		});
 		this.add(saboteur);
 	}
--- src/main/vue/VueDomino.java
+++ src/main/vue/VueDomino.java
@@ -1,29 +1,56 @@
 package main.vue;
 
+import java.awt.GridBagConstraints;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import main.Joueur;
 import main.box.BoxDomino;
 import main.lien.LienDomino;
 import main.planche.PlancheDomino;
 
 @SuppressWarnings("serial")
-public class VueDomino extends Vue {	
-	public VueDomino(PlancheDomino planche) {
-		super(planche);
+public class VueDomino extends Vue {
+	public VueDomino(PlancheDomino planche, Joueur joueur) {
+		super(planche, joueur);
 		this.setTitle("Table Domino");
 	}
-	
+
 	public void addDomino(int x, int y, BoxDomino box) {
-		LienDomino test = (LienDomino) box.getLien();
-		this.plateau[x][y] = new ImagePan("Images/Dice" + test.getNombre() + ".png");
+		LienDomino nombreCase = (LienDomino) box.getLien();
+		this.plateau[x][y] = new ImagePan("Images/Dice" + nombreCase.getNombre() + ".png");
 	}
-	
+
 	public void partie() {
 		this.planche.partie();
-		for(int i = 0; i < this.planche.getTableau().length; i++) {
-			for(int j = 0; j < this.planche.getTableau()[0].length; j++) {
-				if( this.planche.getTableau()[i][j] != null)
+		for (int i = 0; i < this.planche.getTableau().length; i++) {
+			for (int j = 0; j < this.planche.getTableau()[0].length; j++) {
+				if (this.planche.getTableau()[i][j] != null)
 					this.addDomino(i, j, (BoxDomino) this.planche.getTableau()[i][j]);
+			}
+		}
+	}
+	
+	@Override
+	public void initPioche() {
+		this.maPioche = new JPanel();
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.gridwidth = 2;
+		c.gridheight = this.joueur.getMain().size();
+		c.weightx = 2;
+		c.weighty = 1;
+		maPioche.add(new JLabel("Ma pioche"), c);
+		for (int i = 0; i < this.joueur.getMain().size(); i++) {
+			LienDomino l1 = (LienDomino) ((BoxDomino) this.joueur.getMain().get(i)).getLien();
+			LienDomino l2 = (LienDomino) (((BoxDomino) this.joueur.getMain().get(i)).getBox2()).getLien();
+			this.maMain.add(new JPanel[] { new ImagePan("Images/Dice" + l1.getNombre() + ".png"),
+					new ImagePan("Images/Dice" + l2.getNombre() + ".png") });
+			for (int j = 0; j < 2; j++) {
+				c.gridx = j;
+				c.gridy = i;
+				this.maPioche.add(this.maMain.get(i)[j], c);
 			}
 		}
 	}
