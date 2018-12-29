fe6fa1cf8df0694a7237bf6b382e5a6d98014043
Damien <Damien@192.168.1.79>
2018-12-29 22:10:12 +0100

    joue la partie

--- src/main/box/BoxDomino.java
+++ src/main/box/BoxDomino.java
@@ -1,5 +1,7 @@
 package main.box;
 
+import java.util.ArrayList;
+
 import main.lien.LienDomino;
 
 public class BoxDomino extends Box {
@@ -14,8 +16,15 @@ public class BoxDomino extends Box {
 
 	@Override
 	public boolean estPosable(Box[][] tableau, int x, int y, int sens) {
+		boolean b = false;
+		for (int[] cote : autourDesDeux(x, y, sens))
+			if (tableau[cote[0]][cote[1]] != null)
+				b = true;
+		if (!b)
+			return false;
 		int[] c = sensToCoordonnees(sens);
-		return super.estPosable(tableau, x, y, sens) && box2.estPosable(tableau, x + c[0], y + c[1], sens) || super.estPosable(tableau, x + c[0],y + c[1], sens) && box2.estPosable(tableau, x, y, sens);
+		return super.estPosable(tableau, x, y, sens) && box2.estPosable(tableau, x + c[0], y + c[1], sens)
+				|| super.estPosable(tableau, x + c[0], y + c[1], sens) && box2.estPosable(tableau, x, y, sens);
 	}
 
 	@Override
@@ -24,17 +33,65 @@ public class BoxDomino extends Box {
 		if (super.estPosable(tableau, x, y, sens) && box2.estPosable(tableau, x + c[0], y + c[1], sens)) {
 			super.pose(tableau, x, y, sens);
 			box2.pose(tableau, x + c[0], y + c[1], sens);
-		}
-		else {
-			super.pose(tableau, x + c[0],y + c[1], sens);
+		} else {
+			super.pose(tableau, x + c[0], y + c[1], sens);
 			box2.pose(tableau, x, y, sens);
 		}
 
 		this.nbLiens--;
 		box2.nbLiens--;
 	}
-	
+
+	public ArrayList<int[]> autour(int x, int y, int sens) {
+		ArrayList<int[]> autour = new ArrayList<>();
+
+		for (int i = 0; i < 4; i++) {
+			if (i == sens)
+				continue;
+			int[] c = sensToCoordonnees(i);
+			autour.add(new int[] { x + c[0], y + c[1] });
+		}
+
+		return autour;
+	}
+
+	public ArrayList<int[]> autourDesDeux(int x, int y, int sens) {
+		ArrayList<int[]> autour = autour(x, y, sens);
+		int[] c = sensToCoordonnees(sens);
+
+		autour.addAll(autour(x + c[0], y + c[1], (sens + 2) % 4));
+		return autour;
+	}
+
 	public Box getBox2() {
 		return box2;
+	}
+
+	public boolean supperieur(BoxDomino boxDomino) {
+		int max1 = Math.max(((LienDomino) this.lien).getNombre(), ((LienDomino) this.box2.lien).getNombre());
+		int max2 = Math.max(((LienDomino) boxDomino.lien).getNombre(), ((LienDomino) boxDomino.box2.lien).getNombre());
+
+		if (max1 > max2)
+			return true;
+		if (max1 < max2)
+			return false;
+
+		int min1 = Math.min(((LienDomino) this.lien).getNombre(), ((LienDomino) this.box2.lien).getNombre());
+		int min2 = Math.min(((LienDomino) boxDomino.lien).getNombre(), ((LienDomino) boxDomino.box2.lien).getNombre());
+
+		return min1 > min2;
+
+	}
+
+	public static int max(ArrayList<BoxDomino> boxDominos) {
+		
+		if (boxDominos == null || boxDominos.size()==0)
+			throw new RuntimeException();
+
+		int max = 0;
+		for (int i = 0; i < boxDominos.size(); i++)
+			if (boxDominos.get(i).supperieur(boxDominos.get(max)))
+				max = i;
+		return max;
 	}
 }
--- src/main/planche/PlancheDomino.java
+++ src/main/planche/PlancheDomino.java
@@ -1,10 +1,13 @@
 package main.planche;
 
+import java.util.ArrayList;
+
+import main.Demande;
 import main.Joueur;
 import main.box.BoxDomino;
 import main.lien.LienDomino;
 
-public class PlancheDomino extends Planche {
+public class PlancheDomino extends Planche<BoxDomino> {
 
 	public PlancheDomino() {
 		super(30, 30);
@@ -15,15 +18,40 @@ public class PlancheDomino extends Planche {
 	}
 
 	@Override
-	public void addJoueur(Joueur joueur) {
+	public void addJoueur(Joueur<BoxDomino> joueur) {
 		if (this.joueurs.size() <= 2)
 			super.addJoueur(joueur);
 	}
 
 	@Override
-	public void tour() {
+	public void tour(int nb) {
 		// si il ne peut pas jouer il pioche et passe
 		//
+
+		affiche();
+		Joueur<BoxDomino> joueur = joueurs.get(nb);
+		while (true) {
+			Demande d = new Demande();
+			BoxDomino box =  joueur.getBox(d.getNb());
+			if (box.estPosable(tableau, d.getX(), d.getY(), d.getSens())) {
+				joueur.joue(d.getNb()).pose(tableau, d.getX(), d.getY(), d.getSens());
+				break;
+			}
+		}
+	}
+	
+	public void premierTour() {
+		ArrayList<BoxDomino> maxDominoParJoueur = new ArrayList<>();
+		ArrayList<Integer> maxIndexParJoueur = new ArrayList<>();
+		
+		int index;
+		for (Joueur<BoxDomino> joueur : joueurs) {
+			maxIndexParJoueur.add(BoxDomino.max(joueur.getMain()));
+			maxDominoParJoueur.add(joueur.getBox(BoxDomino.max(joueur.getMain())));
+		}
+		joueurCourant = BoxDomino.max(maxDominoParJoueur);
+		
+		joueurs.get(joueurCourant).joue(maxIndexParJoueur.get(joueurCourant)).pose(tableau, 15, 15, 1);
 	}
 
 	@Override
@@ -32,12 +60,27 @@ public class PlancheDomino extends Planche {
 		// pioche a tour de role 7 dominos
 		// le plus gros double le joue (obligatoire)
 		// chaque joueur joue a tour de role
-		/*
-		 * addJoueur(new Joueur("1")); addJoueur(new Joueur("2"));
-		 * 
-		 * joueurs.get(0).pioche(pioche()); joueurs.get(0).pioche(pioche());
-		 * joueurs.get(0).pioche(pioche());
-		 */
+		
+		addJoueur(new Joueur<BoxDomino>("1"));
+		addJoueur(new Joueur<BoxDomino>("2"));
 
+		for (int i=0; i<7; i++)
+			for (Joueur<BoxDomino> joueur : joueurs)
+				joueur.pioche(pioche());
+
+		premierTour();
+		for (int i=0; true; i++) {
+			tour(i%joueurs.size());
+		}
+
+	}
+	
+	public void affiche() {
+		for (int i=0;i<tableau.length; i++) {
+			for (int j=0;j<tableau.length; j++)
+				System.out.print(tableau[j][i]==null?".":tableau[j][i].getLien());
+
+			System.out.println();
+		}
 	}
 }
