d08208834d8cd348c79055304b5d1bca796ea2a6
Damien <Damien@192.168.1.79>
2018-12-29 08:16:33 +0100

    change la définition des box et utilise le tableau

--- src/main/box/Box.java
+++ src/main/box/Box.java
@@ -3,26 +3,22 @@ package main.box;
 import main.lien.Lien;
 
 public class Box {
-	private Box[] boxLiees;
 	private int nbLiensMax;
+	protected int nbLiens;
+	private int sens;
 	protected Lien lien;
 
 	public Box(int nbLiensMax, Lien lien) {
-		this.boxLiees = new Box[4];
 		this.nbLiensMax = nbLiensMax;
+		this.nbLiens = 0;
 		this.lien = lien;
 	}
 
-	public boolean estAjoutable(Box box, int cote1) {
+	public boolean estAjoutable(Box box) {
 
-		if (box.nbLiens() >= box.nbLiensMax)
+		if (box.nbLiens >= box.nbLiensMax)
 			return false;
-		if (this.nbLiens() >= this.nbLiensMax)
-			return false;
-
-		if (this.boxLiees[cote1] != null)
-			return false;
-		if (box.boxLiees[(cote1 + 2) % 4] != null)
+		if (this.nbLiens >= this.nbLiensMax)
 			return false;
 
 		if (!this.lien.equals(box.lien))
@@ -31,29 +27,62 @@ public class Box {
 		return true;
 	}
 
-	public void ajout(Box box, int cote1) {
-
-		this.boxLiees[cote1] = box;
-		box.boxLiees[(cote1 + 2) % 4] = this;
-	}
-	
-	public int nbLiens() {
-		int nb = 0;
-		for (Box case0 : boxLiees)
-			if (case0 != null)
-				nb++;
-		return nb;
+	public boolean estPosable(Box[][] tableau, int x, int y, int sens) {
+		if (tableau[x][y] != null)
+			return false;
+		for (int i = 0; i < 4; i++) {
+			int[] c = sensToCoordonnees(i);
+			if (tableau[x + c[0]][y + c[1]] != null && !estAjoutable(tableau[x + c[0]][y + c[1]]))
+				return false;
+		}
+		return true;
 	}
 
+	public void pose(Box[][] tableau, int x, int y, int sens) {
+		for (int i = 0; i < 4; i++) {
+			int[] c = sensToCoordonnees(i);
+			if (tableau[x + c[0]][y + c[1]] != null) {
+				tableau[x + c[0]][y + c[1]].ajout();
+				ajout();
+			}
+		}
+		setSens(sens);
+		tableau[x][y] = this;
 
-	public Box[] getCasesLiees() {
-		return this.boxLiees;
+	}
+
+	public int[] sensToCoordonnees(int sens) {
+		if (sens == 0)
+			return new int[] { 0, -1 };
+		if (sens == 1)
+			return new int[] { 1, 0 };
+		if (sens == 2)
+			return new int[] { 0, 1 };
+		if (sens == 3)
+			return new int[] { -1, 0 };
+		throw new RuntimeException();
+	}
+
+	public void ajout() {
+		nbLiens++;
+	}
+
+	public void setSens(int sens) {
+		this.sens = sens;
+	}
+
+	public int getSens() {
+		return sens;
 	}
 
 	public int getNbLiensMax() {
 		return this.nbLiensMax;
 	}
-	
+
+	public int getNbLiens() {
+		return this.nbLiens;
+	}
+
 	public Lien getLien() {
 		return this.lien;
 	}
--- src/main/box/BoxDomino.java
+++ src/main/box/BoxDomino.java
@@ -8,24 +8,32 @@ public class BoxDomino extends Box {
 	public BoxDomino(LienDomino lien1, LienDomino lien2) {
 		super(2, lien1);
 		this.box2 = new Box(2, lien2);
+		this.ajout();
+		box2.ajout();
 	}
 
-	public void ajout(Box box, int cote1, int cote2) {
-		if (super.estAjoutable(box, (cote1 + 2) % 4)) {
-			super.ajout(box, (cote1 + 2) % 4);
-			super.ajout(box2, cote2);
-		} else {
-			box2.ajout(box, (cote1 + 2) % 4);
-			box2.ajout((Box) this, cote2);
+	@Override
+	public boolean estPosable(Box[][] tableau, int x, int y, int sens) {
+		int[] c = sensToCoordonnees(sens);
+		return super.estPosable(tableau, x, y, sens) && box2.estPosable(tableau, x + c[0], y + c[1], sens) || super.estPosable(tableau, x + c[0],y + c[1], sens) && box2.estPosable(tableau, x, y, sens);
+	}
+
+	@Override
+	public void pose(Box[][] tableau, int x, int y, int sens) {
+		int[] c = sensToCoordonnees(sens);
+		if (super.estPosable(tableau, x, y, sens) && box2.estPosable(tableau, x + c[0], y + c[1], sens)) {
+			super.pose(tableau, x, y, sens);
+			box2.pose(tableau, x + c[0], y + c[1], sens);
 		}
-	}
+		else {
+			super.pose(tableau, x + c[0],y + c[1], sens);
+			box2.pose(tableau, x, y, sens);
+		}
 
-	public boolean estAjoutable(Box box, int cote1, int cote2) {
-		if (!super.estAjoutable(box, (cote1 + 2) % 4) && !box2.estAjoutable(box, (cote1 + 2) % 4))
-			return false;
-		return true;
+		this.nbLiens--;
+		box2.nbLiens--;
 	}
-
+	
 	public Box getBox2() {
 		return box2;
 	}
