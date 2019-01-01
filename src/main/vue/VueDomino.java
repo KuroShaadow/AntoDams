3d16fa016211f24c29fda0ed2b782f9fa411d654
Kuro <Kuro@Kuro>
2019-01-01 22:08:32 +0100

    réajustement de l'affichage de la pioche et de la taille de la fenetre (s'ajuste automatiquement a la taille de l'écran)

--- src/main/vue/VueDomino.java
+++ src/main/vue/VueDomino.java
@@ -1,6 +1,7 @@
 package main.vue;
 
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -46,12 +47,14 @@ public class VueDomino extends Vue<BoxDomino> {
 		}
 		Joueur<BoxDomino> joueur = this.planche.getJoueur(this.planche.getCourant());
 		maPioche.removeAll();
+		maPioche.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.BOTH;
-		c.gridwidth = 2;
-		c.weightx = 2;
+		c.gridx = 0;
+		c.gridy = 0;
 		c.weighty = 1;
+		c.gridwidth = 2;
 		maPioche.add(new JLabel("Pioche de " + joueur.toString()), c);
+		c.gridwidth = 1;
 		for (int i = 0; i < joueur.getMain().size(); i++) {
 			LienDomino l1 = (LienDomino) ((BoxDomino) joueur.getMain().get(i)).getLien();
 			LienDomino l2 = (LienDomino) (((BoxDomino) joueur.getMain().get(i)).getBox2()).getLien();
@@ -59,7 +62,7 @@ public class VueDomino extends Vue<BoxDomino> {
 					new JLabel(new ImageLab("Images/Dice" + l2.getNombre() + ".png", 50)) });
 			for (int j = 0; j < 2; j++) {
 				c.gridx = j;
-				c.gridy = i;
+				c.gridy = i + 1;
 				this.maPioche.add(this.maMain.get(i)[j], c);
 			}
 		}
