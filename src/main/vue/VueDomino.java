d7a73a10a6024a3dfb1d7cf44118c487d688ef0d
Kuro <Kuro@Kuro>
2019-01-04 16:24:12 +0100

    ajout du bouton piocher

--- src/main/vue/VueDomino.java
+++ src/main/vue/VueDomino.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -82,6 +83,14 @@ public class VueDomino extends Vue<BoxDomino> {
 			this.maMain.add(ligne);
 			this.maPioche.add(ligne, c);
 		}
+		JButton piocher = new JButton("Piocher");
+		piocher.addActionListener(event -> {
+			joueur.pioche(this.planche.pioche());
+			this.updateVue();
+		});
+		c.gridx = 0;
+		c.gridy = joueur.getMain().size() + 1;
+		this.maPioche.add(piocher, c);
 		SwingUtilities.updateComponentTreeUI(this);
 	}
 }
